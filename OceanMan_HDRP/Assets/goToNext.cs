e789500f6fc35198cf9ea1914236ebcfa839a6f3
Lyss <shaitan.lyssm@gmail.com>
2021-01-29 23:55:56 +0100

    crash

---
+++ OceanMan_HDRP/Assets/goToNext.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class goToNext : MonoBehaviour
+{
+    public bool isChoice;
+    private Button button;
+    private Text text;
+    private GameObject neighbor1;
+    private GameObject neighbor2;
+    public bool enable;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        button = gameObject.GetComponent<Button>();
+        text = gameObject.GetComponent<Text>();
+        /*text.enabled = enable;
+        button.enabled = enable;*/
+        button.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isChoice)
+        {
+            neighbor1 = GameObject.Find((int.Parse(gameObject.name) + 1).ToString());
+        }
+    }
+    private void OnClick()
+    {
+        /*button.enabled = false;
+        text.enabled = false;*/
+
+        
+        button = neighbor1.GetComponent<Button>();
+        text = neighbor1.GetComponent<Text>();
+        /*button.enabled = true;
+        text.enabled = true;*/
+    }
+}
