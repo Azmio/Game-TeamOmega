d6a7edf9753b7724b06661bf3ea45c6c94b49aa9
Frosty <aayushshukla11@gmail.com>
2021-02-04 23:10:26 +0530

    UI scene dialogue script fix

--- OceanMan_HDRP/Assets/goToNext.cs
+++ OceanMan_HDRP/Assets/goToNext.cs
@@ -6,12 +6,14 @@ using System;
 
 public class goToNext : MonoBehaviour
 {
-    public bool isChoice;
     private Button button;
     private Text text;
-    private GameObject neighbor1;
-    private GameObject neighbor2;
-    public bool enable;
+    public GameObject nextTextObject;
+
+    private Transform parentTransform;
+    private Text parentText;
+
+    public bool isLast=false;
     
     // Start is called before the first frame update
     void Start()
@@ -22,25 +24,47 @@ public class goToNext : MonoBehaviour
         /*text.enabled = enable;
         button.enabled = enable;*/
         button.onClick.AddListener(OnClick);
+        parentTransform=transform.parent;
+        parentText=parentTransform.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isChoice)
-        {
-            neighbor1 = GameObject.Find((int.Parse(gameObject.name) + 1).ToString());
-        }
+        //if (!isChoice)
+        //{
+        //    neighbor1 = GameObject.Find((int.Parse(gameObject.name) + 1).ToString());
+        //}
+        
     }
     private void OnClick()
     {
         /*button.enabled = false;
         text.enabled = false;*/
-
-        
-        button = neighbor1.GetComponent<Button>();
-        text = neighbor1.GetComponent<Text>();
+        ChangeTextObject(isLast);       
+        //button = neighbor1.GetComponent<Button>();
+        //text = neighbor1.GetComponent<Text>();
         /*button.enabled = true;
         text.enabled = true;*/
+    }
+
+    private void ChangeTextObject(bool last){
+
+        if(HasParent()){
+            parentTransform.gameObject.SetActive(false);
+        }
+        else{
+            this.gameObject.SetActive(false);
+        }
+        if(!last)
+            nextTextObject.SetActive(true);
+        else
+            Debug.Log("Change Scene");
+    }
+
+    private bool HasParent(){
+        if(parentText!=null)
+            return true;
+        return false;
     }
 }
